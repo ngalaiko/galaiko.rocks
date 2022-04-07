9ae79286502d46ba308c66e57ba253857bbd76a6
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-07 20:16:54 +0200

    add list of restaurants and cafes

--- content/_index.md
+++ content/_index.md
@@ -8,15 +8,18 @@ if you want to contact me for any reason, the best way is to send an email to [n
 
 i have a [github][] account if you want to checkout stuff i occasionaly do for fun (programming wise)
 
-there is also [page on this website][] that i use to catalog vinyl that i have
+there is a page on this website with a [list of vinyl records i own][]
+
+and here is a [top 30 restaurants and cafes][] i visited since 2018
 
 ### currently
 
 - living in Stockholm
-- working on rethinking [code collaboration][]
+- working on [code collaboration][]
 
 [nikita@galaiko.rocks]: mailto:nikita@galaiko.rocks
 [github]: https://github.com/ngalaiko
 [pgp key]: /keys/nikita@galaiko.rocks.asc
-[page on this website]: /records/
+[list of vinyl records i own]: /records/
 [code collaboration]: https://getsturdy.com
+[top 30 restaurants and cafes]: /restaurants_and_cafes/
---
+++ scripts/ledger/restaurants_and_cafes.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+import locale
+import subprocess
+import csv
+import os
+from datetime import date
+from decimal import Decimal, getcontext
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--number", default=25)
+parser.add_argument("-f", "--file", default="main.ledger")
+args = parser.parse_args()
+number = int(args.number)
+
+getcontext().prec = 6
+locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')
+locale._override_localeconv = {'mon_thousands_sep': ' '}
+
+command=[
+    'hledger',
+    '-f', args.file,
+    'register',
+    'expenses:Food:Restaurants & Cafes$',
+    '--value', 'then,SEK',
+    '--output-format', 'csv',
+]
+
+output = subprocess.run(command,check=True, stdout=subprocess.PIPE, universal_newlines=True, encoding='utf-8')
+transactions = csv.reader(output.stdout.splitlines()[1:])
+
+def convert(row):
+    return {
+        'date': date.fromisoformat(row[1]),
+        'payee': row[3].split('|')[0].strip(),
+        'amount': Decimal(row[5].split(' ')[0]),
+        'currency': row[5].split(' ')[1]
+    }
+
+transactions = map(convert, transactions)
+
+# calculate top 50 payees by amount sum, and print them like this as json:
+# payee, total amount, currency, number of transactions
+def groupby(transactions, get_key):
+    groups = {}
+    for transaction in transactions:
+        key = get_key(transaction)
+        if key not in groups:
+            groups[key] = {
+                'payee': transaction['payee'],
+                'amount': Decimal(0),
+                'currency': transaction['currency'],
+                'count': 0
+            }
+        groups[key]['amount'] += transaction['amount']
+        groups[key]['count'] += 1
+    return groups
+
+groups = groupby(transactions, lambda t: t['payee'])
+
+result = map(lambda key: groups[key] , sorted(groups, key=lambda g: groups[g]['count'], reverse=True)[:number])
+
+class DecimalEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Decimal):
+      return locale.format_string('%.2f', obj, grouping=True, monetary=True)
+    return json.JSONEncoder.default(self, obj)
+
+script_location = os.path.realpath(__file__)
+script_dir = os.path.dirname(script_location)
+dst = os.path.join(script_dir, "../../data/restaurants_and_cafes.json")
+dst = os.path.normpath(dst)
+content = json.dumps(list(result), indent=4, cls=DecimalEncoder, ensure_ascii=False )
+with open(dst, 'w') as f:
+    f.write(content)
