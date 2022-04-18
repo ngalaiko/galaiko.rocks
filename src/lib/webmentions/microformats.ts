8dff524eca327d3ec67b5e28ec8ca017d611e4f9
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-18 18:20:12 +0200

    webmentions: add mentions and reposts

--- src/lib/webmentions/microformats.ts
+++ src/lib/webmentions/microformats.ts
@@ -20,11 +20,6 @@ const isReplyTo =
 	({ properties }: MicroformatRoot) =>
 		properties['in-reply-to']?.includes(url.href);
 
-const isLikeOf =
-	(url: URL) =>
-	({ properties }: MicroformatRoot) =>
-		properties['like-of']?.includes(url.href) || properties['like']?.includes(url.href);
-
 export const repliesTo = (url: URL) =>
 	accepted
 		.filter(({ target }) => href(target) === url.href)
@@ -32,9 +27,47 @@ export const repliesTo = (url: URL) =>
 		.flatMap(convertToMicroformats)
 		.filter(isReplyTo(url));
 
+const isLikeOf =
+	(url: URL) =>
+	({ properties }: MicroformatRoot) =>
+		properties['like-of']?.includes(url.href) || properties['like']?.includes(url.href);
+
 export const likesOf = (url: URL) =>
 	accepted
 		.filter(({ target }) => href(target) === url.href)
 		.map(({ source }) => source as Parsed)
 		.flatMap(convertToMicroformats)
 		.filter(isLikeOf(url));
+
+const isRepostOf =
+	(url: URL) =>
+	({ properties }: MicroformatRoot) =>
+		properties['repost-of']?.includes(url.href);
+
+export const repostsOf = (url: URL) =>
+	accepted
+		.filter(({ target }) => href(target) === url.href)
+		.map(({ source }) => source as Parsed)
+		.flatMap(convertToMicroformats)
+		.filter(isRepostOf(url));
+
+const not = (predicate: (i: MicroformatRoot) => boolean) => (i: MicroformatRoot) => !predicate(i);
+
+const and =
+	(...predicates: Array<(i: MicroformatRoot) => boolean>) =>
+	(i: MicroformatRoot) =>
+		predicates.every((predicate) => predicate(i));
+
+const containsPropertyValue =
+	(url: URL) =>
+	({ properties }: MicroformatRoot) =>
+		Object.values(properties).some((values) => values.includes(url.href));
+
+export const mentionsOf = (url: URL) =>
+	accepted
+		.filter(({ target }) => href(target) === url.href)
+		.map(({ source }) => source as Parsed)
+		.flatMap(convertToMicroformats)
+		.filter(
+			and(containsPropertyValue(url), not(isReplyTo(url)), not(isLikeOf(url)), not(isRepostOf(url)))
+		);
