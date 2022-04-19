1d3a88dba2080eb3a513428adaa5091142f67042
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-19 12:33:54 +0200

    force reload page when downloading key

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,9 +10,9 @@
 <div class="content h-card">
 	<header class="flex items-center gap-2">
 		<h1 class="p-name text-4xl font-bold flex-1">
-            <span class="p-given-name">Nikita</span> 
-            <span class="p-family-name">Galaiko</span>
-        </h1>
+			<span class="p-given-name">Nikita</span>
+			<span class="p-family-name">Galaiko</span>
+		</h1>
 		<link class="u-url" href="https://galaiko.rocks" />
 		<div class="w-[50px] h-[50px]">
 			<Image
@@ -29,8 +29,11 @@
 
 		<p>
 			if you want to contact me for any reason, the best way is to send an email to
-			<a class="u-email" href="mailto:nikita@galaiko.rocks" rel="me">nikita@galaiko.rocks</a>. consider using my
-			<a class="u-key" href="/keys/nikita@galaiko.rocks.asc" rel="me pgpkey authn">pgp key</a> if you are into it
+			<a class="u-email" href="mailto:nikita@galaiko.rocks" rel="me">nikita@galaiko.rocks</a>.
+			consider using my
+			<a sveltekit:reload class="u-key" href="/keys/nikita@galaiko.rocks.asc" rel="me pgpkey authn"
+				>pgp key</a
+			> if you are into it
 		</p>
 
 		<p>
@@ -49,7 +52,10 @@
 
 		<h3>currently</h3>
 		<ul>
-			<li>living in <span class="p-locality">Stockholm</span>, <span class="p-country-name">Sweden</span></li>
+			<li>
+				living in <span class="p-locality">Stockholm</span>,
+				<span class="p-country-name">Sweden</span>
+			</li>
 			<li>working on <a href="https://getsturdy.com">code collaboration</a></li>
 		</ul>
 	</GlitchyLinks>
