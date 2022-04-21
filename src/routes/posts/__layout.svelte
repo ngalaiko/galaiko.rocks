55d2b2ee650ad585f66a19290013acdc92c1ca55
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-21 19:19:54 +0200

    fetch post webmentions

--- src/routes/posts/__layout.svelte
+++ src/routes/posts/__layout.svelte
@@ -11,18 +11,24 @@
 	// 4. nothing => throw 404
 	export const load: Load = async ({ url, fetch }) => {
 		const res = await fetch(trimRight(url.pathname, '/') + '.json');
-		if (res.ok) {
-			const body = (await res.json()) as Post | Post[];
-			return body instanceof Array
-				? { status: 200, props: { posts: body } }
-				: body.path === url.pathname
-				? { status: 200, props: { post: body } }
-				: { status: 301, redirect: body.path };
-		} else {
+		if (!res.ok)
 			return res.status === 404
 				? { status: 404, error: 'Page not found' }
 				: { status: 500, error: 'Something went wrong' };
-		}
+
+		const body = (await res.json()) as Post | Post[];
+		if (body instanceof Array) return { status: 200, props: { posts: body } };
+		if (body.path !== url.pathname) return { status: 301, redirect: body.path };
+		return {
+			status: 200,
+			props: {
+				post: body,
+				likes: await fetch('likes.json').then((res) => res.json()),
+				replies: await fetch('replies.json').then((res) => res.json()),
+				reposts: await fetch('reposts.json').then((res) => res.json()),
+				mentions: await fetch('mentions.json').then((res) => res.json())
+			}
+		};
 	};
 </script>
 
@@ -31,6 +37,12 @@
 
 	export let post: Post;
 	export let posts: Post[];
+
+    // TODO: add types and display it
+	export let likes: any[];
+	export let replies: any[];
+	export let reposts: any[];
+	export let mentions: any[];
 </script>
 
 {#if post}
