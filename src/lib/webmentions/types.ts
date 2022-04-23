5c0b7b0bc592283ed46b68fd221ed10361df1b2f
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-23 18:46:04 +0200

    process script uses shared parse function

--- src/lib/webmentions/types.ts
+++ src/lib/webmentions/types.ts
@@ -33,3 +33,21 @@ export type Reply = {
 	published?: Date;
 	updated?: Date;
 };
+
+const parsedFromJSON = (i: any): Parsed | URL => {
+	return i instanceof Object
+		? {
+				url: new URL(i.url),
+				contentType: i.contentType,
+				body: i.body
+		  }
+		: new URL(i as string);
+};
+
+export const webmentionFromJSON = (mention: any): Webmention => ({
+	target: parsedFromJSON(mention.target),
+	source: parsedFromJSON(mention.source),
+	timestamp: new Date(mention.timestamp),
+	status: mention.status as Status,
+	id: mention.id
+});
