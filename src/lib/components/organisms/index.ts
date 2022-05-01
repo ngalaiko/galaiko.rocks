265cb1718a5a0e93c9451ec0078cf2d397f8b8fc
Nikita Galaiko <nikita@galaiko.rocks>
2022-05-01 15:45:16 +0200

    minor changes

--- src/lib/components/organisms/index.ts
+++ src/lib/components/organisms/index.ts
@@ -1,4 +1,3 @@
 export { default as Post } from './Post.svelte';
 export { default as Posts } from './Posts.svelte';
-export { default as Reply } from './Reply.svelte';
 export { default as ColorToggle } from './ColorToggle.svelte';
