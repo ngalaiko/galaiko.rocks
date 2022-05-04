2e1b8fedce59677f18af0feae4ad5b44ea6d8e29
Nikita Galaiko <nikita@galaiko.rocks>
2022-05-04 18:41:05 +0200

    markup comments better

--- src/lib/components/organisms/Webmentions.svelte
+++ src/lib/components/organisms/Webmentions.svelte
@@ -29,17 +29,13 @@
 	].sort((a, b) => b.sort.getTime() - a.sort.getTime());
 </script>
 
-<div>
+<section>
 	<h2 class="text-xl">{all.length} mentions{all.length ? ':' : ''}</h2>
-	<ul class="flex flex-col gap-2">
-		{#each all as { component, props }}
-			<li class="py-2">
-				<svelte:component this={component} {...props} />
-			</li>
-		{:else}
-			<li class="py-2">
-				<a sveltekit:reload class="text-blue" href="https://indieweb.org/Webmention">Send one!</a>
-			</li>
-		{/each}
-	</ul>
-</div>
+	{#each all as { component, props }}
+		<article class="py-2">
+			<svelte:component this={component} {...props} />
+		</article>
+	{:else}
+		<a sveltekit:reload class="text-blue" href="https://indieweb.org/Webmention">Send one!</a>
+	{/each}
+</section>
