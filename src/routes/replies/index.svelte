b93f892e18250d3b4f914c2e8a467270546db087
Nikita Galaiko <nikita@galaiko.rocks>
2022-05-07 13:36:51 +0200

    add reply pages

---
+++ src/routes/replies/index.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+	import type { Reply } from '$lib/webmentions';
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ fetch }) => {
+		const res = await fetch('/replies.json');
+		const replies = (await res.json()) as Reply[];
+		return res.ok
+			? { status: 200, props: { replies } }
+			: { status: 500, error: 'Something went wrong' };
+	};
+</script>
+
+<script lang="ts">
+	import WithBorder from '$lib/components/layouts/WithBorder.svelte';
+	import { Reply as ReplyComponent } from '$lib/components/molecules';
+
+	export let replies: Reply[] = [];
+</script>
+
+<WithBorder>
+	{#each replies as reply}
+		<ReplyComponent {reply} detailed />
+	{/each}
+</WithBorder>
