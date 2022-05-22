0a0f3e382f2b46cd52b2bbeeec76ec70e9cd7d2a
Nikita Galaiko <nikita@galaiko.rocks>
2022-05-22 19:51:42 +0200

    cocktails page

--- src/lib/cocktails/index.ts
+++ src/lib/cocktails/index.ts
@@ -1,3 +1,50 @@
-import TheLastWord from './list/The Last Word.cook';
+import type { Step, StepIngredient } from '@cooklang/cooklang-ts';
 
-export default [TheLastWord];
+export type Ingredient = {
+	name: string;
+	quantity?: string;
+};
+
+export type Cocktail = {
+	title: string;
+	ingredients: Ingredient[];
+	steps: string[];
+};
+
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const list = () =>
+	Promise.all(
+		Object.entries(import.meta.glob('./**/*.cook')).map(
+			async ([filename, module]): Promise<Cocktail> => {
+				const m = (await module()).default;
+				const title = filename.split('/').pop().replace('.cook', '');
+				const ingredients = m.steps
+					.flatMap((step: Step) => step.filter((s) => s.type === 'ingredient'))
+					.map(
+						(ingredient: StepIngredient): Ingredient => ({
+							name: capitalize(ingredient.name),
+							quantity:
+								ingredient.quantity !== 1 && ingredient.quantity !== 'some'
+									? `${ingredient.quantity}${ingredient.units}`
+									: undefined
+						})
+					);
+				const steps = m.steps.map((step: Step) =>
+					step.reduce((acc, s) => {
+						switch (s.type) {
+							case 'ingredient':
+								return `${acc}${s.name}`;
+							case 'cookware':
+								return `${acc}${s.name}`;
+							case 'timer':
+								return `${acc}${s.quantity} ${s.units}`;
+							case 'text':
+								return `${acc}${s.value}`;
+						}
+					}, '')
+				);
+				return { title, ingredients, steps };
+			}
+		)
+	).then((cocktails) => cocktails.sort((a, b) => a.title.localeCompare(b.title)));
---
+++ src/routes/cocktails/index.svelte
@@ -0,0 +1,44 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	import type { Cocktail } from '$lib/cocktails';
+
+	export const load: Load = async ({ fetch }) => {
+		const res = await fetch('/cocktails.json');
+		if (!res.ok) return { status: 500, error: 'Something went wrong' };
+		const cocktails = (await res.json()) as Cocktail[];
+		return { status: 200, props: { cocktails } };
+	};
+</script>
+
+<script lang="ts">
+	export let cocktails: Cocktail[];
+
+    const slugify = (s: string) => s.toLowerCase().replace(/\s+/g, '-');
+</script>
+
+<svelte:head>
+	<title>Cocktails</title>
+</svelte:head>
+<ul class="flex flex-col gap-6">
+	{#each cocktails as cocktail}
+        {@const slug = slugify(cocktail.title)}
+		<li class="flex flex-col gap-2">
+            <h2 id={slug}>
+			    <a href={'#' + slug} class="font-semibold underline">{cocktail.title}</a>
+            </h2>
+			<ul>
+				{#each cocktail.ingredients as ingredient}
+					<li>
+						<span>-</span>
+						<span>{ingredient.name}{ingredient.quantity ? ':' : ''}</span>
+						{#if ingredient.quantity}
+							<span>{ingredient.quantity}</span>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+
+			<p>{cocktail.steps.join(' ')}</p>
+		</li>
+	{/each}
+</ul>
