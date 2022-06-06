23a8c5f6a858aa8411374688617e34c8653cd53b
Nikita Galaiko <nikita@galaiko.rocks>
2022-06-06 12:26:12 +0200

    zoomable images

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,18 @@
 	import '../app.css';
 	import { ColorToggle, WithBorder } from '$lib/components';
 	import { Footer } from '$lib/components/molecules';
+	import Lightense from 'lightense-images';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		Lightense(document.querySelectorAll('.zoomable'), {
+			time: 100,
+			keyboard: true,
+			cubicBezier: 'cubic-bezier(.2, 0, .1, 1)',
+			background: 'var(--background)',
+			zIndex: 1e6
+		});
+	});
 </script>
 
 <ColorToggle />
