0144b81cd0cf81cb5aa6c7eef4c3abf8f295d0a6
Nikita Galaiko <nikita@galaiko.rocks>
2022-06-06 12:00:22 +0200

    mr shadow

--- src/lib/components/layouts/WithBorder.svelte
+++ src/lib/components/layouts/WithBorder.svelte
@@ -1,5 +1,3 @@
-<div
-	class="bg-background-soft lg:drop-shadow-xl lg:mt-6 lg:drop-shadow-xl lg:mt-6 text-lg pt-6 pb-12 mx-5 mb-32 md:px-0 md:px-20 md:pt-12"
->
+<div class="bg-background-soft text-lg pt-6 pb-12 mx-5 mb-32 md:px-0 md:px-20 md:pt-12 lg:mt-6">
 	<slot />
 </div>
