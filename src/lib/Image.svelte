811eb00184f8d70bc3206d2e552bbaa8a1f9b459
Nikita Galaiko <nikita@galaiko.rocks>
2022-06-06 13:16:28 +0200

    fix zoom

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -9,6 +9,18 @@
 	const allSources = Array.isArray(src) ? src : [{ srcset: src }];
 	const sources = allSources.slice(0, -1);
 	const lastSource = allSources[allSources.length - 1];
+
+	import Lightense from 'lightense-images';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		Lightense(document.querySelectorAll('.zoomable'), {
+			time: 100,
+			keyboard: true,
+			cubicBezier: 'cubic-bezier(.2, 0, .1, 1)',
+			background: 'var(--background)',
+			zIndex: 1e6
+		});
+	});
 </script>
 
 <picture {...$$restProps} class={className}>
