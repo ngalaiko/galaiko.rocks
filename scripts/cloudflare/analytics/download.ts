d64dac7ba7c644371641c2deecba38163c44c4f4
Nikita Galaiko <nikita@galaiko.rocks>
2022-06-07 21:09:05 +0200

    cloudflare: make some progress

---
+++ scripts/cloudflare/analytics/download.ts
@@ -0,0 +1,120 @@
+import yargs from 'yargs';
+import fetch from 'node-fetch';
+import { addSeconds, differenceInSeconds } from 'date-fns';
+import { writeJSON } from '../../utils.js';
+
+const argv = yargs(process.argv.slice(2))
+	.usage('Usage: $0 <command> [options]')
+	.option('api-token', {
+		alias: 't',
+		describe: 'The token to use for authentication',
+		type: 'string',
+		demandOption: true
+	})
+	.option('email', {
+		alias: 'e',
+		describe: 'The email to use for authentication',
+		type: 'string',
+		demandOption: true
+	})
+	.option('output', {
+		alias: 'o',
+		describe: 'The output file',
+		type: 'string'
+		// demandOption: true
+	});
+
+const variables = ({ from, to }: { from: Date; to: Date }) => ({
+	accountTag: 'd9eda3671dd279e79c22103ce1f879dd',
+	filter: {
+		AND: [
+			{
+				datetime_geq: from.toISOString(),
+				datetime_leq: to.toISOString()
+			},
+			{
+				siteTag_in: ['99fffbfd97b74b1880c23b0617f24aac']
+			}
+		]
+	},
+	order: 'sum_visits_DESC',
+	limit: 1000
+});
+
+const operationName = 'GetRumAnalyticsTopPaths';
+
+const query = `query GetRumAnalyticsTopPaths {
+  viewer {
+    accounts(filter: {accountTag: $accountTag}) {
+      topPaths: rumPageloadEventsAdaptiveGroups(
+        filter: $filter, 
+        limit: $limit, 
+        orderBy: [$order],
+	  ) {
+        count
+        metric: dimensions {
+          requestPath
+        }
+      }
+    }
+  }
+}`;
+
+type Response = {
+	errors?: { code: number; message: string }[];
+	data?: {
+		viewer: {
+			accounts: {
+				topPaths: {
+					count: number;
+					metric: {
+						requestPath: string;
+					};
+				}[];
+			}[];
+		};
+	};
+};
+
+const list = async ({ from, to }: { from: Date; to: Date }): Promise<Response> =>
+	fetch('https://api.cloudflare.com/client/v4/graphql', {
+		method: 'POST',
+		headers: {
+			authorization: `bearer ${argv.argv.apiToken}`,
+			'x-auth-email': argv.argv.email,
+			'content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			query,
+			variables: variables({ from, to }),
+			operationName
+		})
+	}).then(async (resp) => (await resp.json()) as Response);
+
+const generatePeriods = ({ from, to, interval }: { from: Date; to: Date; interval: number }) => {
+	const periods = [];
+	const now = new Date();
+	for (let i = 0; i < Math.ceil(differenceInSeconds(to, from) / interval); i++) {
+		const from = addSeconds(now, -interval * (i + 1));
+		const to = addSeconds(now, -interval * i);
+		periods.push({ from, to });
+	}
+	return periods;
+};
+
+Promise.all(
+	generatePeriods({
+		from: new Date('2022-04-03T15:47:36.061747Z'),
+		to: new Date(),
+		interval: 3600 * 24 // 1 day
+	}).map(async ({ from, to }) => {
+		const { data, errors } = await list({ from, to });
+		if (errors) throw new Error(errors.map((e) => e.message).join('\n'));
+		if (!data) throw new Error('No data');
+		const metrics = data.viewer.accounts[0].topPaths.reduce(
+			(acc, { metric: { requestPath }, count }) => ({ ...acc, [requestPath]: count }),
+			{}
+		);
+		return { from, to, metrics };
+	})
+).then(console.log);
