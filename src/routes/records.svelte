b2ae07eec2fe0ac1dfafb22b0d879d046e6f97ed
Nikita Galaiko <nikita@galaiko.rocks>
2022-06-15 15:23:43 +0200

    better html markup for /records

--- src/routes/records.svelte
+++ src/routes/records.svelte
@@ -6,16 +6,20 @@
 	<title>Records</title>
 </svelte:head>
 
-<article class="grid grid-cols-3 md:grid-cols-4 gap-1">
-	{#each records as record}
-		{@const info = record['basic_information']}
-		{@const artist = info['artists'][0]}
-		{@const fullName = `${artist.name} ${info.title}`}
-		{@const discogsUrl = `https://www.discogs.com/release/${info.id}`}
-		{#if info.thumb}
-			<a href={discogsUrl}>
-				<img src={info.cover_image} alt={fullName} lazy={true} />
-			</a>
-		{/if}
-	{/each}
+<article>
+	<ul class="grid grid-cols-3 md:grid-cols-4 gap-1">
+		{#each records as record}
+			{@const info = record['basic_information']}
+			{@const artist = info['artists'][0]}
+			{@const fullName = `${artist.name} ${info.title}`}
+			{@const discogsUrl = `https://www.discogs.com/release/${info.id}`}
+			{#if info.thumb}
+				<li>
+					<a href={discogsUrl}>
+						<img src={info.cover_image} alt={fullName} lazy={true} />
+					</a>
+				</li>
+			{/if}
+		{/each}
+	</ul>
 </article>
