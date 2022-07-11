ea0bb251b5bc7108e9fae345cef9e39a00200d0b
Nikita Galaiko <nikita@galaiko.rocks>
2022-07-11 13:57:45 +0200

    rm number from restaurants and cafes

--- src/routes/restaurants_and_cafes.svelte
+++ src/routes/restaurants_and_cafes.svelte
@@ -10,14 +10,12 @@
 	<table class="table-auto w-full">
 		<thead>
 			<tr class="text-gray text-md font-semibold text-left">
-				<th scope="col" />
-				<th scope="col" class="py-3 px-2">Place</th>
-				<th scope="col" class="py-3 px-2">Times</th>
-				<th scope="col" class="py-3 px-2">Spent</th>
+				<th scope="col" class="pt-3 px-2">Place</th>
+				<th scope="col" class="pt-3 px-2">Times</th>
+				<th scope="col" class="pt-3 px-2">Spent</th>
 			</tr>
-			{#each restaurantsAndCafes as { payee, count, amount, currency }, index}
+			{#each restaurantsAndCafes as { payee, count, amount, currency }}
 				<tr>
-					<td class="whitespace-nowrap px-2 md:px-3 py-4">{index + 1}</td>
 					<td class="md:whitespace-nowrap px-2 md:px-3 py-4">{payee}</td>
 					<td class="whitespace-nowrap px-2 md:px-3 py-4">{count}</td>
 					<td class="whitespace-nowrap px-2 md:px-3 py-4">{amount.toLocaleString()} {currency}</td>
