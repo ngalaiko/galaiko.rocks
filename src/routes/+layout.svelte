634638fb1beea0c2ec0e599502c074f424537184
Nikita Galaiko <nikita@galaiko.rocks>
2022-08-26 08:52:38 +0200

    Revert "vercel: send analytics"
    This reverts commit d883358ae5dc7f0b09c08e126fe9222f88f436cf.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,6 @@
 	import '../app.css';
 	import { WithBorder } from '$lib/components';
 	import { Footer } from '$lib/components/molecules';
-	import { webVitals } from '$lib/vitals';
-	import { browser, dev } from '$app/env';
-	import { page } from '$app/stores';
-
-	const analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-	$: if (!dev && browser && analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId
-		});
-	}
 </script>
 
 <main id="main" class="mx-auto max-w-3xl min-h-screen flex flex-col justify-between">
