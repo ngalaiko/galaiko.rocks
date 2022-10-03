957c222eee8f09d23db4eaae0782738e64b6aec8
Nikita Galaiko <nikita@galaiko.rocks>
2022-10-03 20:54:00 +0200

    fix prerendering for non static builds

--- svelte.config.js
+++ svelte.config.js
@@ -7,35 +7,35 @@ import autoLinkHeadings from 'rehype-autolink-headings';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [
-		mdsvex({
-			extensions: ['.md'],
-			smartypants: {
-				dashes: 'oldschool'
-			},
-			rehypePlugins: [
-				slug, // adds slug to headers
-				[autoLinkHeadings, { behavior: 'wrap' }] //  adds a <a> around slugged headers
-			]
-		}),
-		preprocess({
-			postcss: true,
-			typescript: true,
-			replace: [['import.meta.env.GITHUB_TOKEN', JSON.stringify(process.env.GITHUB_TOKEN)]]
-		})
-	],
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			enabled: true,
-			crawl: true,
-			entries: [
-				'/posts/' //  it's not linked from anywhere
-			]
-		},
-		trailingSlash: 'always'
-	}
+    extensions: ['.svelte', '.md'],
+    preprocess: [
+        mdsvex({
+            extensions: ['.md'],
+            smartypants: {
+                dashes: 'oldschool'
+            },
+            rehypePlugins: [
+                slug, // adds slug to headers
+                [autoLinkHeadings, { behavior: 'wrap' }] //  adds a <a> around slugged headers
+            ]
+        }),
+        preprocess({
+            postcss: true,
+            typescript: true,
+            replace: [['import.meta.env.GITHUB_TOKEN', JSON.stringify(process.env.GITHUB_TOKEN)]]
+        })
+    ],
+    kit: {
+        adapter: adapter(),
+        prerender: {
+            enabled: true,
+            crawl: true,
+            entries: [
+                '/posts/' //  it's not linked from anywhere
+            ]
+        },
+        trailingSlash: 'always'
+    }
 };
 
 export default config;
