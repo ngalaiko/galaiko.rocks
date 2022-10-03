488510320aca1506194ae1e531122a96521195c6
Nikita Galaiko <nikita@galaiko.rocks>
2022-10-03 14:28:37 +0200

    remove components

--- src/lib/components/layouts/WithBorder.svelte
+++
@@ -1,3 +0,0 @@
-<div class="bg-background-soft text-lg pt-6 pb-12 mx-5 mb-32 md:px-0 md:px-20 md:pt-12 lg:mt-6">
-	<slot />
-</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
 	import '../app.css';
-	import { WithBorder } from '$lib/components';
-	import { Footer } from '$lib/components/molecules';
 </script>
 
 <main id="main" class="mx-auto max-w-3xl min-h-screen flex flex-col justify-between">
-	<WithBorder>
+	<div class="bg-background-soft text-lg pt-6 pb-12 mx-5 mb-32 md:px-20 md:pt-12 lg:mt-6">
 		<slot />
-	</WithBorder>
-	<Footer />
+	</div>
+
+	<footer class="p-2 opacity-50 text-center">
+		<a href="/">Nikita Galaiko</a>
+		<span>&#183;</span>
+		<a data-sveltekit-reload href="https://creativecommons.org/licenses/by-nc/4.0/" rel="license"
+			>CC BY-NC 4.0</a
+		>
+		<span>&#183;</span>
+		<span>2018...{new Date().getFullYear()}</span>
+		<span>&#183;</span>
+		<a rel="privacy" href="/privacy/">privacy policy</a>
+		<span>&#183;</span>
+		<a data-sveltekit-reload href="https://github.com/ngalaiko/galaiko.rocks">source</a>
+	</footer>
 </main>
