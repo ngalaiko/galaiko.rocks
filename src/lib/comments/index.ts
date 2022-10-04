a4ffb6c06d9f05bcdb215646d8881682cbe48759
Nikita Galaiko <nikita@galaiko.rocks>
2022-10-04 11:28:18 +0200

    render comments

---
+++ src/lib/comments/index.ts
@@ -0,0 +1,34 @@
+import { compareDesc } from 'date-fns';
+
+export type Comment = {
+    pathname: string;
+    created: Date;
+    authorName: string;
+    default: any;
+};
+
+export const findByPathname = async (pathname: string) =>
+    list().then((comments) =>
+        comments
+            .filter((c) => c.pathname === pathname)
+            .sort((a, b) => compareDesc(a.created, b.created))
+    );
+
+const filenameToPath = (filename: string) =>
+    filename.split('/').slice(0, -1).join('/').slice(1) + '/';
+
+export const list = () =>
+    Promise.all(
+        Object.entries(import.meta.glob('./**/*.md')).map(
+            async ([filename, module]): Promise<Comment> => {
+                const m = await module();
+                const { metadata } = m;
+                return {
+                    default: m.default,
+                    authorName: metadata.author_name,
+                    pathname: filenameToPath(filename),
+                    created: new Date(metadata.timestamp * 1000)
+                };
+            }
+        )
+    );
