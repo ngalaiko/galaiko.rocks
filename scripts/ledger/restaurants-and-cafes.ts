64013c39210eaf2e2b558acaaea5fefc1f4b5533
Nikita Galaiko <nikita@galaiko.rocks>
2022-10-05 16:33:39 +0200

    render places map

--- scripts/ledger/restaurants-and-cafes.ts
+++ scripts/ledger/restaurants-and-cafes.ts
@@ -5,97 +5,104 @@ import currency from 'currency.js';
 import { addYears } from 'date-fns';
 import yargs from 'yargs';
 import { writeJSON } from '../utils.js';
+import locations from './locations.js';
 
 const argv = yargs(process.argv.slice(2))
-	.usage('Usage: $0 <command> [options]')
-	.option('file', {
-		alias: 'f',
-		type: 'string',
-		description: 'The ledger file to use'
-	})
-	.option('number', {
-		alias: 'n',
-		type: 'number',
-		description: 'Number of places to export'
-	})
-	.option('output', {
-		alias: 'o',
-		type: 'string',
-		description: 'Output file',
-		demandOption: true
-	})
-	.parseSync();
+    .usage('Usage: $0 <command> [options]')
+    .option('file', {
+        alias: 'f',
+        type: 'string',
+        description: 'The ledger file to use'
+    })
+    .option('number', {
+        alias: 'n',
+        type: 'number',
+        description: 'Minimum number of visits'
+    })
+    .option('output', {
+        alias: 'o',
+        type: 'string',
+        description: 'Output file',
+        demandOption: true
+    })
+    .parseSync();
 
 type Transaction = { date: Date; amount: currency; payee: string; currency: string };
 
 const log = (...args: any[]) => {
-	console.log(...args);
-	return args;
+    console.log(...args);
+    return args;
 };
 
 const hledger = (args: string[]): Promise<any[]> =>
-	new Promise((resolve, reject) => {
-		const rows = [];
-		const parseCSV = csv
-			.parse({ headers: true })
-			.on('error', reject)
-			.on('data', (row) => rows.push(row))
-			.on('end', () => resolve(rows));
-		const child = spawn('hledger', [...args]);
-		let error = '';
-		child.stderr.on('data', (data) => (error += data));
-		child.stderr.on('end', () => {
-			if (error) {
-				reject(error);
-			}
-		});
-		child.stdout.pipe(parseCSV);
-	});
+    new Promise((resolve, reject) => {
+        const rows = [];
+        const parseCSV = csv
+            .parse({ headers: true })
+            .on('error', reject)
+            .on('data', (row) => rows.push(row))
+            .on('end', () => resolve(rows));
+        const child = spawn('hledger', [...args]);
+        let error = '';
+        child.stderr.on('data', (data) => (error += data));
+        child.stderr.on('end', () => {
+            if (error) {
+                reject(error);
+            }
+        });
+        child.stdout.pipe(parseCSV);
+    });
 
 const exportTransactions = async (file?: string) => {
-	log('exporting transactions...');
-	const withFile = file ? ['--file', file] : [];
-	const params = [
-		'register',
-		'--value=then,SEK',
-		'--output-format=csv',
-		'expenses:Food:Restaurants & Cafes$',
-		'--begin',
-		addYears(new Date(), -1).toISOString().slice(0, 10)
-	];
-	return await hledger(params.concat(withFile)).then((rows) =>
-		rows.map((row: any): Transaction => {
-			const { date, description, amount } = row;
-			const amountParts = amount.split(' ');
-			return {
-				date: startOfDay(new Date(date)),
-				payee: description.split('|')[0],
-				amount: currency(amountParts[0]),
-				currency: amountParts[1]
-			};
-		})
-	);
+    log('exporting transactions...');
+    const withFile = file ? ['--file', file] : [];
+    const params = [
+        'register',
+        '--value=then,SEK',
+        '--output-format=csv',
+        'expenses:Food:Restaurants & Cafes$',
+        '--begin',
+        addYears(new Date(), -1).toISOString().slice(0, 10)
+    ];
+    return await hledger(params.concat(withFile)).then((rows) =>
+        rows.map((row: any): Transaction => {
+            const { date, description, amount } = row;
+            const amountParts = amount.split(' ');
+            return {
+                date: startOfDay(new Date(date)),
+                payee: description.split('|')[0],
+                amount: currency(amountParts[0]),
+                currency: amountParts[1]
+            };
+        })
+    );
 };
 
 export const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
-	list.reduce((previous, currentItem) => {
-		const group = getKey(currentItem);
-		if (!previous[group]) previous[group] = [];
-		previous[group].push(currentItem);
-		return previous;
-	}, {} as Record<K, T[]>);
+    list.reduce((previous, currentItem) => {
+        const group = getKey(currentItem);
+        if (!previous[group]) previous[group] = [];
+        previous[group].push(currentItem);
+        return previous;
+    }, {} as Record<K, T[]>);
 
 exportTransactions(argv.file)
-	.then((transactions) =>
-		Object.entries(groupBy(transactions, (t: Transaction) => t.payee))
-			.map(([payee, transactions]) => ({
-				payee,
-				count: transactions.length,
-				currency: transactions[0].currency,
-				amount: transactions.reduce((sum, t) => sum.add(t.amount), currency(0))
-			}))
-			.sort((a, b) => b.count - a.count)
-			.slice(0, argv.number || 10)
-	)
-	.then(writeJSON(argv.output))
-	.then(() => console.log('done!'));
+    .then((transactions) =>
+        Object.entries(groupBy(transactions, (t: Transaction) => t.payee))
+            .map(([payee, transactions]) => ({
+                payee,
+                // filter out negative transactions as they are refunds
+                count: transactions.filter((t) => t.amount.intValue > 0).length,
+                currency: transactions[0].currency,
+                amount: transactions.reduce((sum, t) => sum.add(t.amount), currency(0))
+            }))
+            .filter((e) => e.count >= argv.number)
+            .sort((a, b) => b.count - a.count)
+            .map((entry) => {
+                const location = locations[entry.payee];
+                if (!location) throw new Error(`${entry.payee}: location missing`);
+                return { ...entry, location };
+            })
+    )
+    .then(writeJSON(argv.output))
+    .then(() => console.log('done!'));
--- src/routes/restaurants_and_cafes/+page.svelte
+++ src/routes/restaurants_and_cafes/+page.svelte
@@ -1,8 +1,58 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { addYears, formatRelative } from 'date-fns';
+	import type { Map } from 'leaflet';
 
 	export let data: PageData;
+
+	const map = (map: HTMLElement) => {
+		let m: Map;
+		import('leaflet/dist/leaflet.css').then(async () => {
+			const L = await import('leaflet');
+
+			// expand the div to make it visible when js is on
+			map.classList.add('h-[400px]');
+
+			// init map with stockholm in the middle
+			m = L.map(map, {
+				center: [59.32799, 18.05467],
+				zoom: 12
+			});
+
+			// use carto maps as a base layer
+			L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+				attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+		    &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+				subdomains: 'abcd',
+				maxZoom: 16
+			}).addTo(m);
+
+			const max = data.places.map((p) => p.count).sort((a, b) => b - a)[0];
+
+			data.places.forEach((place) => {
+				const div = document.createElement('div');
+				L.marker(place.location as [number, number], {
+					icon: L.divIcon({
+						html: div,
+						className: `rounded-full h-4 w-4 bg-orange`
+					}),
+					title: place.payee,
+					alt: place.payee,
+					riseOnHover: true
+				})
+					.setOpacity(0.2 + (place.count / max) * 0.8)
+					.bindPopup(
+						[
+							`<b>${place.payee}</b>`,
+							`visits: ${place.count}`,
+							`spent: ${place.amount} ${place.currency}`
+						].join('<br/>')
+					)
+					.addTo(m);
+			});
+		});
+		return { destroy: () => m.remove() };
+	};
 </script>
 
 <svelte:head>
@@ -17,6 +67,9 @@
 			new Date()
 		)}:
 	</p>
+
+	<div use:map />
+
 	<table class="table-auto w-full">
 		<thead>
 			<tr class="text-gray text-md font-semibold text-left">
