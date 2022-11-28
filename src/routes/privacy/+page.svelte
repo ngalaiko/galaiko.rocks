82c68300ddb1bc4735147e36896e81fda6fea314
Nikita Galaiko <nikita@galaiko.rocks>
2022-11-28 21:08:32 +0100

    Update +page.svelte

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -36,12 +36,6 @@
 		> analytics.
 	</p>
 
-	<p>
-		You can see all the data it has on the
-		<a href="https://umami.galaiko.rocks/share/6GLJwDAP/galaiko.rocks">public dashboard</a>. If you
-		don't want to be tracked, make sure your browser has 'Do not track' preference enabled.
-	</p>
-
 	<h2>Cookies</h2>
 	<p>No cookies usage.</p>
 
