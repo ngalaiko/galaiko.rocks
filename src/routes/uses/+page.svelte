eb87d99eac18adf2e1f0fe68f837efafc9b41126
Nikita Galaiko <nikita@galaiko.rocks>
2022-12-27 13:12:46 +0100

    add /uses/

---
+++ src/routes/uses/+page.svelte
@@ -0,0 +1,211 @@
+<svelte:head>
+	<title>uses</title>
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Uses" />
+	<meta property="og:description" content="what i use" />
+</svelte:head>
+
+<arcicle>
+	<h1>/uses/</h1>
+
+	<p>on this page you can find most of the things that i use.</p>
+
+	<h2>software</h2>
+	<ul>
+		<li>
+			<h3><a href="https://neovim.io/">neovim</a></h3>
+			<p>
+				vim's modal editing model is really the way to write any kind of text. neovim is a great
+				modern implementation of that.
+			</p>
+			<p>
+				here is my <a href="https://github.com/ngalaiko/dotfiles/tree/master/.config/nvim"
+					>configuration</a
+				>
+			</p>
+		</li>
+
+		<li>
+			<h3><a href="https://sw.kovidgoyal.net/kitty/">kitty</a></h3>
+			<p>gpu based terminal emulator that has replaces tmux for me completely.</p>
+			<p>
+				<a href="https://github.com/ngalaiko/dotfiles/tree/master/.config/kitty"
+					>highly configurable</a
+				>, too
+			</p>
+			<p />
+		</li>
+
+		<li>
+			<h3><a href="http://raycast.com">raycast</a></h3>
+			<p>a very nice spotlight/alfred alternative for mac os</p>
+		</li>
+
+		<li>
+			<h3><a href="https://www.zsh.org"> zsh</a></h3>
+			<p>a sane shell with a lot of integrations</p>
+			<p>
+				<a href="https://github.com/ngalaiko/dotfiles/tree/master/.config/zsh">here</a> are my confings
+			</p>
+		</li>
+
+		<li>
+			<h3><a href="https://ohmyposh.dev">ohmyposh</a></h3>
+			<p>
+				a fast prompt renderer for my terminal. pretty easy to configure and it has a lot of useful
+				modules.
+			</p>
+			<p>
+				my configs live <a href="https://github.com/ngalaiko/dotfiles/tree/master/.config/ohmyposh"
+					>here</a
+				>
+			</p>
+		</li>
+
+		<li>
+			<h3><a href="https://calibre-ebook.com">calible</a></h3>
+			<p>just a great e-book management software</p>
+		</li>
+
+		<li>
+			<h3><a href="https://hledger.org">hledger</a></h3>
+			<p>i keep track of all of my personal finances using this software.</p>
+		</li>
+
+		<li>
+			<h3><a href="http://koreader.rocks">koreader</a></h3>
+			<p>a great e-book reader that i run on my e-book</p>
+		</li>
+
+		<li>
+			<h3><a href="https://netnewswire.com">net news wire</a></h3>
+			<p>i mainly read stuff on the interenet via rrs, and this is the best rss reader i know.</p>
+		</li>
+
+		<li>
+			<h3><a href="https://overcast.fm/">overcast</a></h3>
+			<p>the best podcast player i've tried.</p>
+		</li>
+
+		<li>
+			<h3>apple mail / notes / calendar / maps</h3>
+			<p>
+				i prefer to not use 3rd party apps when i don't have to, and default apple software covers
+				all my needs when it comes to email/notes/calendar and maps.
+			</p>
+		</li>
+
+		<li>
+			<h3>
+				<a href="https://organicmaps.app">organic maps</a>
+			</h3>
+			<p>sometimes i need to use offline maps though, then i use organic maps.</p>
+		</li>
+	</ul>
+
+	<h2>services</h2>
+	<ul>
+		<li>
+			<h3><a href="https://nextdns.io/?from=7dkc9vrh">nextdns</a></h3>
+			<p>
+				a hosted alternative for pihole. it blocks dns of known analytic tools, ads and stuff like
+				that. very nice. i run it everywhere: laptop, mobile phone, my home network.
+			</p>
+		</li>
+
+		<li>
+			<h3><a href="https://countermail.com">countermail</a></h3>
+			<p>hosted swedish privacy friendly email provider</p>
+		</li>
+
+		<li>
+			<h3><a href="https://telegram.com">telegram</a></h3>
+			<p>maybe not the most private messenger, but the ux is unbeatable</p>
+		</li>
+
+		<li>
+			<h3><a href="https://notifier.in">notifier.in</a></h3>
+			<p>if i want to subscribe to a newsletter, it goes to my rss reader too.</p>
+		</li>
+
+		<li>
+			<h3><a href="https://mullvad.net">mullvad</a></h3>
+			<p>vpn provider with a great approach to protecting user's privacy</p>
+		</li>
+
+		<li>
+			<h3><a href="https://tailscale.com"> tailscale</a></h3>
+			<p>all my devices and home network can talk to each other via this amazing vpn</p>
+		</li>
+	</ul>
+
+	<h2>hardware</h2>
+	<ul>
+		<li>
+			<h3>14 inch macbook pro 2021</h3>
+			<p>fullmetal body + m1 chip</p>
+		</li>
+
+		<li>
+			<h3>iphone 13 mini</h3>
+			<p>mini size is very handy.</p>
+		</li>
+
+		<li>
+			<h3>air pods pro 2</h3>
+			<p>love the size, noice cancelling - perfect for podcasts on the go.</p>
+		</li>
+
+		<li>
+			<h3>
+				<a href="https://store.ui.com/collections/unifi-network-unifi-os-consoles/products/udm-us"
+					>unify dream machine</a
+				>
+			</h3>
+			<p>
+				great router with a lot of features. i also use it to run nextdns and tailscale for the
+				local home network.
+			</p>
+		</li>
+
+		<li>
+			<h3>corne lp</h3>
+			<p>
+				got the pcb and the case from <a
+					href="https://boardsource.xyz/store/5f2efc462902de7151495057">boardsource</a
+				>, swiches from
+				<a href="https://lowprokb.ca/products/sunset-tactile-choc-switches">lowprokb</a>,
+				<a href="https://lowprokb.ca/products/ldsa-low-profile-blank-keycaps">keycaps</a> from them too.
+			</p>
+
+			<p>split + 40% size + lowprofile + tactile = my choice</p>
+		</li>
+
+		<li>
+			<h3>playstation 5</h3>
+			<p>🎮</p>
+		</li>
+
+		<li>
+			<h3>
+				<a href="https://www.audio-technica.com/en-us/at-lp120xusb">audio technica lp120xUSB</a>
+			</h3>
+			<p>i listen to my <a href="/records/">vinyl</a> with it</p>
+		</li>
+		<li>
+			<h3><a href="https://us.kobobooks.com/products/kobo-libra-h2o">kobo libra h2o</a></h3>
+			<p>amazon free, great format, great size</p>
+		</li>
+	</ul>
+
+	<h3>transport</h3>
+	<ul>
+		<li>
+			<h3><a href="https://www.brompton.com">brompton m6l</a></h3>
+			<p>
+				i don't use public transport much, but when i need to, i can take the bike inside. looks
+				good too.
+			</p>
+		</li>
+	</ul>
+</arcicle>
