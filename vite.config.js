db424f6a5efd676016d7871778069b31df7b0dcf
Nikita Galaiko <nikita@galaiko.rocks>
2023-04-02 21:00:15 +0200

    re-enable remote assets

--- vite.config.js
+++ vite.config.js
@@ -29,16 +29,17 @@ const config = {
         pluginRawFont(['.ttf']),
         process.env.NODE_ENV == 'production'
             ? [
-                // remoteAssets({
-                //     assetsDir: 'src/lib/records/covers',
-                //     awaitDownload: true,
-                //     resolveMode: 'relative',
-                //     rules: [
-                //         {
-                //             match: /https:\/\/i\.discogs\.com.*?\.(?:png|jpeg|jpg)/gi
-                //         }
-                //     ]
-                // })
+                remoteAssets({
+                    assetsDir: 'src/lib/records/covers',
+                    awaitDownload: true,
+                    retryTooManyRequests: true,
+                    resolveMode: 'relative',
+                    rules: [
+                        {
+                            match: /https:\/\/i\.discogs\.com.*?\.(?:png|jpeg|jpg)/gi
+                        }
+                    ]
+                })
             ]
             : [],
         imagePresets({
