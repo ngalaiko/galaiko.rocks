8f46b2e9faa077dc758e207c2b47a60a2eabc8cc
Nikita Galaiko <nikita@galaiko.rocks>
2023-08-05 15:34:56 +0200

    rm console.log

--- plugins/vite-plugin-b64.ts
+++ plugins/vite-plugin-b64.ts
@@ -2,9 +2,8 @@ import { readFileSync } from 'fs';
 
 export default () => ({
   name: 'vite-b64-plugin',
-  transform: (code: string, id: string) => {
+  transform: (_code: string, id: string) => {
     if (!id.match(/\?b64$/)) return;
-    console.log(id, code);
     var path = id.replace(/\?b64/, '');
     var data = readFileSync(path, 'base64');
     return `export default '${data}'`;
