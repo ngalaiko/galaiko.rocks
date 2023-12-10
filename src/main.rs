be747602626fc5dfb91d518ff0df7b6450ac02b8
Nikita Galaiko <nikita@galaiko.rocks>
2023-12-10 16:41:37 +0100

    watch

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::{collections::HashMap, sync::Arc};
-
-use self::path::normalize;
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     femme::start();
 
-    let mut app = tide::with_state(State::build()?);
+    let state = State::build()?;
+    let state = Arc::new(RwLock::new(state));
+
+    #[cfg(debug_assertions)]
+    let _watcher = watch_changes(state.clone())?;
+
+    let mut app = tide::with_state(state);
     app.with(tide::log::LogMiddleware::new());
     app.at("/").get(serve_asset);
     app.at("/*path").get(serve_asset);
@@ -16,14 +23,29 @@ async fn main() -> tide::Result<()> {
     Ok(())
 }
 
-async fn serve_asset(req: tide::Request<State>) -> tide::Result {
+#[cfg(debug_assertions)]
+fn watch_changes(state: Arc<RwLock<State>>) -> notify::Result<notify::RecommendedWatcher> {
+    use notify::Watcher;
+
+    let mut watcher = notify::recommended_watcher(move |_| {
+        *state.write().unwrap() = State::build().unwrap();
+    })?;
+    watcher.watch(
+        std::path::Path::new("./assets"),
+        notify::RecursiveMode::Recursive,
+    )?;
+    Ok(watcher)
+}
+
+async fn serve_asset(req: tide::Request<Arc<RwLock<State>>>) -> tide::Result {
     let requested_path = req.url().path();
     let requested_path = urlencoding::decode(requested_path)
         .map_err(|error| tide::Error::new(tide::StatusCode::BadRequest, error))?;
 
-    let normalized_path = normalize(requested_path.to_string());
+    let normalized_path = path::normalize(requested_path.to_string());
 
-    if let Some(response) = req.state().0.get(&normalized_path) {
+    let state = req.state().read().expect("Error reading state");
+    if let Some(response) = state.0.get(&normalized_path) {
         tide::Response::try_from(response)
     } else {
         Ok(tide::Response::new(tide::StatusCode::NotFound))
