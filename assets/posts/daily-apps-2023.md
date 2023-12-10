52455c71dd7cd32212d48db134368a771a16b3e6
Nikita Galaiko <nikita@galaiko.rocks>
2023-12-10 11:52:17 +0100

    refactor

---
+++ assets/posts/daily-apps-2023.md
@@ -0,0 +1,39 @@
+---
+title: "My daily apps at the end of 2023"
+date: 2023-11-07
+aliases:
+  - "/posts/blog/my-default-apps-at-the-end-of-2023/"
+---
+
+jumping on a [hype train](https://defaults.rknight.me) here:
+
+|                                   |                                        |
+| --------------------------------- | -------------------------------------- |
+| 📨 Mail Client                    | Mail.app                               |
+| 📮 Mail Server                    | [countermail](https://countermail.com) |
+| 📝 Notes                          | Notes.app                              |
+| ✅ To-Do                          | Reminders.app                          |
+| 📷 iPhone Photo Shooting          | \_                                     |
+| 🟦 Photo Management               | \_                                     |
+| 📆 Calendar                       | Calendar.app                           |
+| 📁 Cloud File Storage             | iCloud                                 |
+| 📖 RSS                            | [NetNewsWire](https://netnewswire.com) |
+| 🙍🏻‍♂️ Contacts                       | Contacts.app                           |
+| 🌐 Browser                        | Safari                                 |
+| 💬 Chat                           | [Telegram](https://telegram.org)       |
+| 🔖 Bookmarks                      | \_                                     |
+| 📑 Read It Later                  | \_                                     |
+| 📜 Word Processing                | vim, Pages.app                         |
+| 📈 Spreadsheets                   | Numbers.app                            |
+| 📊 Presentations                  | Keynote.app                            |
+| 🛒 Shopping Lists                 | Reminders.app                          |
+| 🍴 Meal Planning                  | \_                                     |
+| 💰 Budgeting and Personal Finance | [hledger](https://hledger.org)         |
+| 📰 News                           | \_                                     |
+| 🎵 Music                          | Apple Music                            |
+| 🎤 Podcasts                       | [Overcast](https://overcast.fm)        |
+| 🔐 Password Management            | [Strongbox](https://strongboxsafe.com) |
+
+one thing I've discovered browsing through other people's default apps - there are mostly no apps i haven't heard about.
+
+which either means I spend too much time in the Internet, or that there are not so many good apps after all.
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::{collections::HashMap, sync::Arc};
+
+use self::path::normalize;
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     femme::start();
 
-    let mut app = tide::new();
+    let mut app = tide::with_state(State::build()?);
     app.with(tide::log::LogMiddleware::new());
     app.at("/").get(serve_asset);
     app.at("/*path").get(serve_asset);
@@ -12,47 +16,125 @@ async fn main() -> tide::Result<()> {
     Ok(())
 }
 
-async fn serve_asset(req: tide::Request<()>) -> tide::Result {
+async fn serve_asset(req: tide::Request<State>) -> tide::Result {
     let requested_path = req.url().path();
     let requested_path = urlencoding::decode(requested_path)
         .map_err(|error| tide::Error::new(tide::StatusCode::BadRequest, error))?;
 
-    let path = path::normalize(&requested_path);
-    dbg!(&requested_path, &path);
-    if path == path::normalize("/posts/index.html") {
-        let page = build_page(&pages::posts());
-        let body = tide::Body::from(page.into_string());
-        Ok(tide::Response::builder(tide::StatusCode::Ok)
-            .header("content-type", "text/html; charset=utf-8")
-            .body(body)
-            .build())
-    } else {
-        let asset = assets::get(&path).map_err(|error| match error {
-            assets::GetAssetError::NotFound(_) => {
-                tide::Error::new(tide::StatusCode::NotFound, error)
-            }
-            assets::GetAssetError::Utf8Error(error) => {
-                tide::Error::new(tide::StatusCode::InternalServerError, error)
-            }
-        })?;
+    let normalized_path = normalize(requested_path.to_string());
 
-        match asset.content {
-            assets::Content::Html(content) => {
-                let page = build_page(&content);
-                let body = tide::Body::from(page.into_string());
+    if let Some(response) = req.state().0.get(&normalized_path) {
+        tide::Response::try_from(response)
+    } else {
+        Ok(tide::Response::new(tide::StatusCode::NotFound))
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Response {
+    Redirect(std::path::PathBuf),
+    Asset(assets::Asset),
+}
+
+impl TryFrom<&Response> for tide::Response {
+    type Error = tide::Error;
+
+    fn try_from(value: &Response) -> Result<Self, Self::Error> {
+        match value {
+            Response::Redirect(path) => {
+                let mut response = tide::Response::new(tide::StatusCode::MovedPermanently);
+                response.insert_header("Location", path.to_str().unwrap());
+                Ok(response)
+            }
+            Response::Asset(
+                assets::Asset::Markdown { html } | assets::Asset::Generated { html },
+            ) => {
+                let html = build_page(html);
+                let body = tide::Body::from(html.clone().into_string());
                 Ok(tide::Response::builder(tide::StatusCode::Ok)
-                    .header("content-type", "text/html; charset=utf-8")
+                    .header("content-type", "text/html")
                     .body(body)
                     .build())
             }
-            assets::Content::Binary { content_type, body } => {
-                let body = tide::Body::from(body);
+            Response::Asset(assets::Asset::Post(post)) => {
+                let html = build_page(&post.html);
+                let body = tide::Body::from(html.into_string());
                 Ok(tide::Response::builder(tide::StatusCode::Ok)
-                    .header("content-type", content_type)
+                    .header("content-type", "text/html")
+                    .body(body)
+                    .build())
+            }
+            Response::Asset(assets::Asset::Other { mimetype, data }) => {
+                let body = tide::Body::from(data.to_owned());
+                Ok(tide::Response::builder(tide::StatusCode::Ok)
+                    .header("content-type", mimetype)
                     .body(body)
                     .build())
             }
         }
+    }
+}
+
+#[derive(Clone)]
+struct State(Arc<HashMap<std::path::PathBuf, Response>>);
+
+#[derive(Debug)]
+pub enum BuildError {
+    GetAssetError(std::path::PathBuf, assets::GetAssetError),
+}
+
+impl std::fmt::Display for BuildError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BuildError::GetAssetError(path, error) => {
+                write!(f, "Error getting asset {}: {}", path.display(), error)
+            }
+        }
+    }
+}
+
+impl std::error::Error for BuildError {}
+
+impl State {
+    fn build() -> Result<Self, BuildError> {
+        let assets = assets::iter()
+            .map(|asset_path| {
+                assets::get(&asset_path)
+                    .map_err(|error| BuildError::GetAssetError(asset_path.clone(), error))
+                    .map(|asset| (asset_path, asset))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        let mut routes = HashMap::new();
+
+        for (asset_path, asset) in assets {
+            if let assets::Asset::Post(post) = &asset {
+                for alias in &post.frontmatter.aliases {
+                    routes.insert(
+                        path::normalize(alias),
+                        Response::Redirect(asset_path.clone()),
+                    );
+                }
+            }
+            routes.insert(asset_path, Response::Asset(asset));
+        }
+
+        routes.insert(
+            std::path::PathBuf::from("/posts/index.html"),
+            Response::Asset(pages::posts(
+                &mut routes
+                    .iter()
+                    .filter_map(|(path, asset)| match asset {
+                        Response::Asset(assets::Asset::Post(post)) => {
+                            Some((path.clone(), post.clone()))
+                        }
+                        _ => None,
+                    })
+                    .collect::<Vec<_>>(),
+            )),
+        );
+
+        Ok(Self(Arc::new(routes)))
     }
 }
 
@@ -73,8 +155,8 @@ fn build_page(content: &maud::Markup) -> maud::Markup {
 }
 
 mod path {
-    pub fn normalize(s: &str) -> std::path::PathBuf {
-        let mut path = std::path::PathBuf::from(s);
+    pub fn normalize<P: AsRef<std::path::Path>>(s: P) -> std::path::PathBuf {
+        let mut path = s.as_ref().to_path_buf();
         if !path.has_root() {
             path = std::path::PathBuf::from("/").join(path);
         };
@@ -134,7 +216,9 @@ mod path {
 }
 
 mod assets {
-    use std::path::{Path, PathBuf};
+    use std::path::PathBuf;
+
+    use crate::markdown;
 
     use super::path;
 
@@ -143,94 +227,143 @@ mod assets {
     struct Assets;
 
     pub fn iter() -> impl Iterator<Item = PathBuf> {
-        Assets::iter().map(|asset_path| path::normalize(&asset_path))
+        Assets::iter().map(|asset_path| path::normalize(asset_path.to_string()))
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Post {
+        pub frontmatter: markdown::Frontmatter,
+        pub html: maud::Markup,
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum Asset {
+        Post(Post),
+        Markdown { html: maud::Markup },
+        Generated { html: maud::Markup },
+        Other { mimetype: String, data: Vec<u8> },
     }
 
     pub fn get(path: &PathBuf) -> Result<Asset, GetAssetError> {
         let asset_path = Assets::iter()
-            .find(|asset_path| path::normalize(asset_path).eq(path))
-            .ok_or_else(|| GetAssetError::NotFound(path.clone()))?;
+            .find(|asset_path| path::normalize(asset_path.to_string()).eq(path))
+            .ok_or(GetAssetError::NotFound)?;
 
         let embedded_file =
             Assets::get(&asset_path).expect("Assets::iter() returned a non-existent path");
 
-        let title = if asset_path.ends_with("index.md") {
-            path.parent()
-        } else {
-            Some(path.as_path())
+        match embedded_file.metadata.mimetype() {
+            "text/markdown" if path.starts_with("/posts") => {
+                let (frontmatter, md) = markdown::extract_frontmatter(&embedded_file.data)
+                    .map_err(GetAssetError::Frontmatter)?;
+                let html = markdown::to_html(path, &md).expect("Error parsing markdown");
+                Ok(Asset::Post(Post { frontmatter, html }))
+            }
+            "text/markdown" => {
+                let md = embedded_file.data.to_vec();
+                let html = markdown::to_html(path, &md).map_err(GetAssetError::ToHtml)?;
+                Ok(Asset::Markdown { html })
+            }
+            mimetype => Ok(Asset::Other {
+                mimetype: mimetype.to_string(),
+                data: embedded_file.data.to_vec(),
+            }),
         }
-        .and_then(|p| p.file_stem())
-        .and_then(|s| s.to_str())
-        .map(ToString::to_string);
-
-        let asset = Asset {
-            title,
-            path: path.clone(),
-            created: embedded_file.metadata.created().map_or(
-                chrono::NaiveDate::from_ymd_opt(1970, 1, 1).expect("valid date"),
-                |created_ts| {
-                    chrono::naive::NaiveDateTime::from_timestamp_opt(
-                        created_ts.try_into().unwrap(),
-                        0,
-                    )
-                    .expect("valid timestamp")
-                    .date()
-                },
-            ),
-            content: if asset_path.ends_with("md") {
-                let html = convert_md(path.parent().expect("expected parent"), &embedded_file.data)
-                    .map_err(GetAssetError::Utf8Error)?;
-                Content::Html(html)
-            } else {
-                Content::Binary {
-                    content_type: embedded_file.metadata.mimetype().to_string(),
-                    body: embedded_file.data.to_vec(),
-                }
-            },
-        };
-
-        Ok(asset)
     }
 
     #[derive(Debug)]
     pub enum GetAssetError {
-        NotFound(PathBuf),
-        Utf8Error(std::str::Utf8Error),
+        NotFound,
+        ToHtml(markdown::ToHtmlError),
+        Frontmatter(markdown::FrontmatterError),
     }
 
     impl std::fmt::Display for GetAssetError {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             match self {
-                GetAssetError::NotFound(path) => {
-                    write!(f, "Asset not found: {}", path.to_str().unwrap())
+                GetAssetError::NotFound => {
+                    write!(f, "Asset not found")
                 }
-                GetAssetError::Utf8Error(err) => write!(f, "UTF-8 error: {err}"),
+                GetAssetError::ToHtml(error) => {
+                    write!(f, "Error parsing markdown: {error}")
+                }
+                GetAssetError::Frontmatter(error) => {
+                    write!(f, "Error parsing frontmatter: {error}")
+                }
             }
         }
     }
 
     impl std::error::Error for GetAssetError {}
+}
 
-    #[derive(Debug)]
-    pub struct Asset {
-        pub title: Option<String>,
-        pub path: PathBuf,
-        pub content: Content,
-        pub created: chrono::NaiveDate,
+mod markdown {
+    use crate::path;
+
+    #[derive(Debug, Clone, serde::Deserialize)]
+    pub struct Frontmatter {
+        pub title: String,
+        pub date: chrono::NaiveDate,
+        pub aliases: Vec<std::path::PathBuf>,
     }
 
     #[derive(Debug)]
-    pub enum Content {
-        Html(maud::Markup),
-        Binary { content_type: String, body: Vec<u8> },
+    pub enum FrontmatterError {
+        SerdeYaml(serde_yaml::Error),
     }
 
-    fn fix_link(root: &Path, link: &str) -> String {
-        root.join(link).display().to_string()
+    impl std::fmt::Display for FrontmatterError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                FrontmatterError::SerdeYaml(error) => write!(f, "{error}"),
+            }
+        }
     }
 
-    fn convert_md(root: &Path, data: &[u8]) -> Result<maud::Markup, std::str::Utf8Error> {
-        let md = std::str::from_utf8(data)?;
+    impl std::error::Error for FrontmatterError {}
+
+    /// Extracts the frontmatter from a markdown file.
+    /// Returns parsed frontmatter and the remaining markdown.
+    pub fn extract_frontmatter(
+        markdown: &[u8],
+    ) -> Result<(Frontmatter, Vec<u8>), FrontmatterError> {
+        let lines = markdown.split(|b| *b == b'\n');
+        let mut frontmatter = Vec::new();
+        let mut markdown = Vec::new();
+        let mut in_frontmatter = false;
+        for line in lines {
+            if line == b"---" {
+                in_frontmatter = !in_frontmatter;
+            } else if in_frontmatter {
+                frontmatter.extend_from_slice(line);
+                frontmatter.push(b'\n');
+            } else {
+                markdown.extend_from_slice(line);
+                markdown.push(b'\n');
+            }
+        }
+        let frontmatter =
+            serde_yaml::from_slice(&frontmatter).map_err(FrontmatterError::SerdeYaml)?;
+        Ok((frontmatter, markdown))
+    }
+
+    #[derive(Debug)]
+    pub enum ToHtmlError {
+        Utf8(std::str::Utf8Error),
+    }
+
+    impl std::fmt::Display for ToHtmlError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                ToHtmlError::Utf8(error) => write!(f, "{error}"),
+            }
+        }
+    }
+
+    impl std::error::Error for ToHtmlError {}
+
+    pub fn to_html(root: &std::path::Path, data: &[u8]) -> Result<maud::Markup, ToHtmlError> {
+        let md = std::str::from_utf8(data).map_err(ToHtmlError::Utf8)?;
         let parser = pulldown_cmark::Parser::new(md);
         let parser = parser.map(|event| match event {
             pulldown_cmark::Event::Start(pulldown_cmark::Tag::Image(typ, link, title)) => {
@@ -267,35 +400,42 @@ mod assets {
         pulldown_cmark::html::push_html(&mut html, parser);
         Ok(maud::PreEscaped(html))
     }
+
+    fn fix_link(root: &std::path::Path, link: &str) -> String {
+        let link = path::normalize(link);
+        root.parent()
+            .unwrap()
+            .join(link)
+            .to_string_lossy()
+            .to_string()
+    }
 }
 
 mod pages {
     use super::assets;
 
-    pub fn posts() -> maud::Markup {
-        let mut posts = assets::iter()
-            .filter(|asset_path| asset_path.starts_with("/posts"))
-            .filter(|asset_path| asset_path.extension().and_then(|e| e.to_str()) == Some("html"))
-            .filter_map(|asset_path| assets::get(&asset_path).ok())
-            .collect::<Vec<_>>();
-        posts.sort_by(|a, b| b.created.cmp(&a.created));
-        maud::html! {
-            h1 {
-                "Posts"
-            }
-            ul {
-                @for post in posts {
-                    li {
-                        a href=(post.path.display()) {
-                            (post.title.unwrap_or_else(|| "Untitled".to_string()))
-                        }
-                        " "
-                        time datetime=(post.created.format("%Y-%m-%d")) {
-                            (post.created.format("%Y-%m-%d"))
+    pub fn posts(posts: &mut [(std::path::PathBuf, assets::Post)]) -> assets::Asset {
+        posts.sort_by(|(_, a), (_, b)| b.frontmatter.date.cmp(&a.frontmatter.date));
+
+        assets::Asset::Generated {
+            html: maud::html! {
+                h1 {
+                    "Posts"
+                }
+                ul {
+                    @for (path, post) in posts {
+                        li {
+                            a href=(path.display()) {
+                                (post.frontmatter.title)
+                            }
+                            " "
+                            time datetime=(post.frontmatter.date.format("%Y-%m-%d")) {
+                                (post.frontmatter.date.format("%Y-%m-%d"))
+                            }
                         }
                     }
                 }
-            }
+            },
         }
     }
 }
