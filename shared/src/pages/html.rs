6c94f4da2765d5e85832f33c811ce6a81b30aa54
Nikita Galaiko <nikita@galaiko.rocks>
2024-01-08 15:57:25 +0100

    refactor

--- shared/src/pages/html.rs
+++ shared/src/pages/html.rs
@@ -1,4 +1,4 @@
-use crate::types::{cocktails, entries, generated, movies, places, records};
+use crate::types::{cocktails, entries, movies, places, records};
 
 fn footer_without_copy_right() -> maud::Markup {
     maud::html! {
@@ -65,36 +65,12 @@ fn new(
 }
 
 #[must_use]
-pub fn cocktails(cocktails: &[cocktails::Cocktail]) -> maud::Markup {
-    new(
-        "cocktails",
-        Some(&maud::html! {
-            link rel="stylesheet" href="/styles/grid.css";
-        }),
-        &generated::cocktails(cocktails),
-        &footer_without_copy_right(),
-    )
-}
-
-#[must_use]
 pub fn cocktail(cocktail: &cocktails::Cocktail) -> maud::Markup {
     new(
         &cocktail.frontmatter.title,
         None,
         &cocktail.body,
         &footer_without_copy_right(),
-    )
-}
-
-#[must_use]
-pub fn posts(posts: &[entries::Entry]) -> maud::Markup {
-    new(
-        "posts",
-        Some(&maud::html! {
-            link rel="alternate" type="application/atom+xml" href="/posts/index.atom";
-        }),
-        &generated::posts(posts),
-        &footer_with_copy_right(),
     )
 }
 
@@ -119,35 +95,199 @@ pub fn entry(page: &entries::Entry) -> maud::Markup {
 }
 
 #[must_use]
-pub fn movies(movies: &[movies::Entry]) -> maud::Markup {
+pub fn posts(posts: &[entries::Entry]) -> maud::Markup {
+    let mut posts = posts.to_vec();
+    posts.sort_by(|a, b| b.frontmatter.date.cmp(&a.frontmatter.date));
+
+    let html = maud::html! {
+        ul {
+            @for post in posts {
+                li {
+                    @if let Some(date) = post.frontmatter.date.map(|date| date.format("%Y-%m-%d")) {
+                        time datetime=(date) {
+                            (date)
+                        }
+                        " | "
+                    }
+                    a href=(post.path.display()) {
+                        (post.frontmatter.title)
+                    }
+                }
+            }
+        }
+    };
+
     new(
-        "movies",
-        None,
-        &generated::movies(movies),
+        "posts",
+        Some(&maud::html! {
+            link rel="alternate" type="application/atom+xml" href="/posts/index.atom";
+        }),
+        &html,
+        &footer_with_copy_right(),
+    )
+}
+
+#[must_use]
+pub fn cocktails(cocktails: &[cocktails::Cocktail]) -> maud::Markup {
+    let mut cocktails = cocktails.to_vec();
+    cocktails.sort_by(|a, b| a.frontmatter.title.cmp(&b.frontmatter.title));
+
+    let html = maud::html! {
+        ul {
+            @for cocktail in cocktails {
+                li {
+                    a href=(cocktail.path.display()) {
+                        figure {
+                            img src=(format!("./{}.jpeg", cocktail.frontmatter.title)) loading="lazy" alt=(cocktail.frontmatter.title);
+                            figcaption {
+                                (cocktail.frontmatter.title)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    new(
+        "cocktails",
+        Some(&maud::html! {
+            link rel="stylesheet" href="/styles/grid.css";
+        }),
+        &html,
         &footer_without_copy_right(),
     )
 }
 
 #[must_use]
+pub fn movies(movies: &[movies::Entry]) -> maud::Markup {
+    let mut movies = movies.to_vec();
+    movies.sort_by(|a, b| b.date.cmp(&a.date));
+
+    let html = maud::html! {
+        ul {
+            @for movie in movies {
+                li {
+                    (movie.date.format("%Y-%m-%d"))
+                    " | "
+                    a href=(movie.href) {
+                        (movie.title)
+                    }
+                    @if movie.is_liked {
+                        " ♥"
+                    }
+                    @if movie.is_rewatch {
+                        " ↻"
+                    }
+                }
+            }
+        }
+    };
+
+    new("movies", None, &html, &footer_without_copy_right())
+}
+
+#[must_use]
 pub fn records(records: &[records::Record]) -> maud::Markup {
+    let mut records = records.to_vec();
+    records.sort_by(|a, b| {
+        let artist_a = &a.basic_information.artists[0]
+            .name
+            .strip_prefix("The ")
+            .unwrap_or(&a.basic_information.artists[0].name);
+        let artist_b = &b.basic_information.artists[0]
+            .name
+            .strip_prefix("The ")
+            .unwrap_or(&b.basic_information.artists[0].name);
+        artist_a.cmp(artist_b)
+    });
+
+    let records = records.iter().filter_map(|record| {
+        std::path::Path::new(&record.basic_information.cover_image)
+            .extension()
+            .and_then(|ext| ext.to_str())
+            .map(|ext| {
+                (
+                    record.id,
+                    record.basic_information.artists[0].name.clone(),
+                    record.basic_information.title.clone(),
+                    format!(
+                        "./{}.{ext}",
+                        record.basic_information.title.replace('/', "-")
+                    ),
+                )
+            })
+    });
+
+    let html = maud::html! {
+        ul {
+            @for (id, artist, title, cover_href) in records {
+                li {
+                    a href=(format!("https://www.discogs.com/release/{}", id)) {
+                        figure {
+                            img src=(cover_href) loading="lazy" alt=(title);
+                            figcaption {
+                                center {
+                                    span { (artist) }
+                                    br;
+                                    span { (title) }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
     new(
         "records",
         Some(&maud::html! {
             link rel="stylesheet" href="/styles/grid.css";
         }),
-        &generated::records(records),
+        &html,
         &footer_without_copy_right(),
     )
 }
 
 #[must_use]
 pub fn places(places: &[places::Place]) -> maud::Markup {
+    let mut places = places.to_vec();
+    places.sort_by(|a, b| b.times.cmp(&a.times));
+
+    let html = maud::html! {
+        table {
+            thead {
+                tr {
+                    th {"name"}
+                    th {"times"}
+                    th {"spent"}
+                }
+            }
+            tbody {
+                @for place in places {
+                    tr {
+                        td {
+                            (place.name)
+                        }
+                        td {
+                            (place.times)
+                        }
+                        td {
+                            (place.spent) " SEK"
+                        }
+                    }
+                }
+            }
+        }
+    };
+
     new(
         "places",
         Some(&maud::html! {
             link rel="stylesheet" href="/styles/table.css";
         }),
-        &generated::places(places),
+        &html,
         &footer_without_copy_right(),
     )
 }
--- shared/src/types.rs
+++ shared/src/types.rs
@@ -1,6 +1,5 @@
 pub mod cocktails;
 pub mod entries;
-pub mod generated;
 pub mod movies;
 pub mod places;
 pub mod records;
