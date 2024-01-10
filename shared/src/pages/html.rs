f3c3abdcac774c0d85ee2e461be4ae030e250c02
Nikita Galaiko <nikita@galaiko.rocks>
2024-01-10 14:12:26 +0100

    fix css

--- shared/src/pages/html.rs
+++ shared/src/pages/html.rs
@@ -40,7 +40,8 @@ fn new(
             link rel="icon" sizes="16x16" href="/favicon-16x16.png";
             link rel="manifest" href="/site.webmanifest";
 
-            link rel="stylesheet" href=(format!("/index.css?v={}",chrono::Local::now().timestamp()));
+            link rel="stylesheet" href=(format!("/fonts/index.css?v={}", chrono::Local::now().timestamp()));
+            link rel="stylesheet" href=(format!("/index.css?v={}", chrono::Local::now().timestamp()));
 
             @if let Some(header) = header {
                 (header)
