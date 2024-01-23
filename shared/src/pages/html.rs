42b45df25992661a267219e03281bb9c35c6605f
Nikita Galaiko <nikita@galaiko.rocks>
2024-01-23 10:07:26 +0100

    csp

--- shared/src/pages/html.rs
+++ shared/src/pages/html.rs
@@ -35,6 +35,8 @@ fn new(
             meta charset="utf-8";
             meta name="viewport" content="width=device-width, initial-scale=1.0";
 
+            meta http-equiv="Content-Security-Policy" content="default-src 'self';";
+
             link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png";
             link rel="icon" sizes="32x32" href="/favicon-32x32.png";
             link rel="icon" sizes="16x16" href="/favicon-16x16.png";
