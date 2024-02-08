03d99c7f902ccee6c532c72507b29bc46b4578aa
Nikita Galaiko <nikita@galaiko.rocks>
2024-02-08 14:47:24 +0100

    update locations

--- update/src/hledger.rs
+++ update/src/hledger.rs
@@ -334,6 +334,7 @@ static LOCATIONS: once_cell::sync::Lazy<HashMap<String, (f64, f64)>> =
         locations.insert("Kuro - Tbilisi".to_string(), (41.706_772, 44.781_744));
         locations.insert("Evolushi".to_string(), (57.708_039, 11.985_960));
         locations.insert("Nonna".to_string(), (57.703_262, 11.959_582));
+        locations.insert("Kastello".to_string(), (57.696_235, 11.952_310))
 
         locations
     });
