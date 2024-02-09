abbc790448ee403e420b7592a126dabb71db887e
Nikita Galaiko <nikita@galaiko.rocks>
2024-02-09 14:54:15 +0100

    use smol in serve

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -1,109 +1,117 @@
+use async_io::Async;
+use axum::extract::Host;
+use axum::http::uri::Authority;
+use axum::http::{header, StatusCode, Uri};
+use axum::response::{IntoResponse, Redirect};
+use axum::{routing::get, Router};
 use clap::Parser;
+use macro_rules_attribute::apply;
+use tower_http::compression::CompressionLayer;
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+
+use std::io;
+use std::net::{SocketAddr, TcpListener};
+use std::sync::Arc;
 
 use shared::path;
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(long, default_value = "127.0.0.1:8080")]
-    address: String,
+    #[arg(long, default_value = "127.0.0.1:8080", value_parser = parse_address)]
+    address: SocketAddr,
 }
 
-#[async_std::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    femme::start();
+fn parse_address(s: &str) -> Result<SocketAddr, String> {
+    s.parse().map_err(|_| format!("invalid address: {s}"))
+}
 
-    let cli = Cli::parse();
-    serve(&cli.address).await
+#[apply(smol_macros::main!)]
+async fn main(ex: &Arc<smol_macros::Executor<'_>>) -> io::Result<()> {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+    let serve = Cli::parse();
+    let comression_layer: CompressionLayer = CompressionLayer::new()
+        .br(true)
+        .deflate(true)
+        .gzip(true)
+        .zstd(true);
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/*path", get(handler))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        )
+        .layer(comression_layer);
+    let listener = Async::<TcpListener>::bind(serve.address).unwrap();
+    smol_axum::serve(ex.clone(), listener, app).await
 }
 
 #[derive(rust_embed::RustEmbed)]
 #[folder = "public/"]
 struct Public;
 
-async fn serve(address: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut app = tide::new();
-    app.with(tide::log::LogMiddleware::new());
-    #[cfg(not(debug_assertions))]
-    app.with(HostnameRedirectMiddleware);
-    app.with(tide_compress::CompressMiddleware::new());
-    app.at("/").get(serve_asset);
-    app.at("/*path").get(serve_asset);
-    app.listen(address).await?;
-    Ok(())
-}
+async fn handler(
+    headers: axum::http::HeaderMap,
+    uri: Uri,
+    Host(hostname): Host,
+) -> impl IntoResponse {
+    if !cfg!(debug_assertions) && hostname != "nikita.galaiko.rocks" {
+        let mut parts = uri.into_parts();
+        parts.scheme = Some("https".parse().expect("always valid"));
+        parts.authority = Some(Authority::from_static("nikita.galaiko.rocks"));
+        let uri = Uri::from_parts(parts).expect("always valid");
+        return Redirect::to(&uri.to_string()).into_response();
+    }
 
-async fn serve_asset(req: tide::Request<()>) -> tide::Result {
-    let requested_path = req.url().path();
-    let requested_path = urlencoding::decode(requested_path)
-        .map_err(|error| tide::Error::new(tide::StatusCode::BadRequest, error))?;
+    let Ok(requested_path) = urlencoding::decode(uri.path()) else {
+        return (StatusCode::BAD_REQUEST, "400 Bad Request").into_response();
+    };
 
     let normalized_path = path::normalize(requested_path.to_string());
-    if requested_path.to_string() != normalized_path.display().to_string() {
-        let response: tide::Response =
-            tide::Redirect::new(normalized_path.display().to_string()).into();
-        return Ok(response);
-    };
+    if normalized_path.display().to_string() != requested_path {
+        return Redirect::to(normalized_path.display().to_string().as_str()).into_response();
+    }
 
     let asset_path = Public::iter()
         .find(|asset_path| path::normalize(asset_path.to_string()) == normalized_path);
-
-    let response =
-        if let Some(embedded_file) = asset_path.and_then(|asset_path| Public::get(&asset_path)) {
-            let etag = etag_header(embedded_file.metadata.sha256_hash());
-            let is_modified = req
-                .header("if-none-match")
-                .map(|etags| etags.contains(&etag))
-                .unwrap_or_default();
-            let content_type = mime_guess::from_path(normalized_path)
-                .first_or_octet_stream()
-                .to_string();
-            if is_modified {
-                tide::Response::builder(tide::StatusCode::NotModified)
-            } else {
-                tide::Response::builder(tide::StatusCode::Ok)
-                    .body(tide::Body::from(embedded_file.data.to_vec()))
-            }
-            .header("content-type", content_type.to_string())
-            .header(
-                "cache-control",
+    if let Some(embedded_file) = asset_path.and_then(|asset_path| Public::get(&asset_path)) {
+        let etag = etag_header(embedded_file.metadata.sha256_hash());
+        let is_modified = headers
+            .get(header::IF_NONE_MATCH)
+            .and_then(|value| value.to_str().ok())
+            .map_or(false, |value| value.contains(&etag));
+        let content_type = mime_guess::from_path(normalized_path)
+            .first_or_octet_stream()
+            .to_string();
+        let headers = [
+            (header::ETAG, etag),
+            (
+                header::CACHE_CONTROL,
                 match content_type.as_str() {
-                    "text/html" => "no-cache, max-age=31536000",
-                    _ => "max-age=31536000",
+                    "text/html" => "no-cache, max-age=31536000".to_string(),
+                    _ => "max-age=31536000".to_string(),
                 },
-            )
-            .header("etag", etag)
-            .build()
+            ),
+            (header::CONTENT_TYPE, content_type),
+        ];
+        if is_modified {
+            (StatusCode::NOT_MODIFIED, headers).into_response()
         } else {
-            tide::Response::new(tide::StatusCode::NotFound)
-        };
-
-    Ok(response)
-}
-
-fn etag_header(hash: [u8; 32]) -> tide::http::headers::HeaderValue {
-    tide::http::headers::HeaderValue::from_bytes(
-        format!(
-            "W\\\"{:x}{:x}{:x}{:x}-{:x}{:x}\"",
-            hash[0], hash[1], hash[2], hash[3], hash[30], hash[31],
-        )
-        .as_bytes()
-        .to_vec(),
-    )
-    .expect("sha256 hash is valid ASCII")
-}
-
-struct HostnameRedirectMiddleware;
-
-#[tide::utils::async_trait]
-impl tide::Middleware<()> for HostnameRedirectMiddleware {
-    async fn handle(&self, request: tide::Request<()>, next: tide::Next<'_, ()>) -> tide::Result {
-        if let Some("nikita.galaiko.rocks") = request.host() {
-            let response: tide::Response = next.run(request).await;
-            Ok(response)
-        } else {
-            let location = format!("https://nikita.galaiko.rocks{}", request.url().path());
-            let response: tide::Response = tide::Redirect::new(location).into();
-            Ok(response)
+            (StatusCode::OK, headers, embedded_file.data).into_response()
         }
+    } else {
+        (StatusCode::NOT_FOUND, "404 Not Found").into_response()
     }
+}
+
+fn etag_header(hash: [u8; 32]) -> String {
+    format!(
+        "W\\\"{:x}{:x}{:x}{:x}-{:x}{:x}\"",
+        hash[0], hash[1], hash[2], hash[3], hash[30], hash[31],
+    )
 }
