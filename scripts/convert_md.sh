ba4e8f8e0a91f3a50c23ada27bd1762307165365
Nikita Galaiko <nikita@galaiko.rocks>
2024-06-06 16:25:21 +0200

    more robust identation for posts

--- scripts/convert_md.sh
+++ scripts/convert_md.sh
@@ -7,10 +7,12 @@ set -o pipefail
 DIR="$(dirname $(readlink -f -- $0))"
 
 frontmatter="$(cat "$1" | sed -n '/^---$/,/^---$/p' | sed '1d;$d')"
-body="$(cat "$1" | sed -e '/^---$/,/^---$/d' | pandoc --lua-filter="$DIR/../filters/fix_images.lua" --lua-filter="$DIR/../filters/fix_links.lua" | sed 's/^/  /')"
+body="$(cat "$1" | sed -e '/^---$/,/^---$/d' | pandoc --lua-filter="$DIR/../filters/fix_images.lua" --lua-filter="$DIR/../filters/fix_links.lua")"
 
 # TODO: this is kind of obscure. any simpler way to pass this path into the template?
 echo "path: ${1/assets\/posts\//}"
 echo "$frontmatter"
 echo "content: |"
-echo "$body"
+echo "$body" | while IFS= read -r line; do
+    echo "  $line"
+done 
