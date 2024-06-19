0f1f09c8e667dda9979b89b1aeaad93325b0609a
Nikita Galaiko <nikita@galaiko.rocks>
2024-06-19 21:50:44 +0200

    unite pandoc filters

--- filters/fix_md.lua
+++ filters/fix_md.lua
@@ -1,9 +1,15 @@
--- Replace '.cook' and '.md' extensions in local links to '.html'
+-- Point all image links to resized versions.
+function Image(el)
+	el.src = el.src .. ".800x0@2x.webp"
+	el.attributes["width"] = "800px"
+	return el
+end
 
 local function replace_extension(s, ext)
 	return s:gsub("%.%w+$", ext)
 end
 
+-- Replace '.cook' and '.md' extensions in local links to '.html'
 function Link(el)
 	if el.target:match("^%./.*%.cook$") then
 		el.target = replace_extension(el.target, ".html")
--- scripts/convert_md.sh
+++ scripts/convert_md.sh
@@ -7,7 +7,7 @@ set -o pipefail
 DIR="$(dirname $(readlink -f -- $0))"
 
 frontmatter="$(cat "$1" | sed -n '/^---$/,/^---$/p' | sed '1d;$d')"
-body="$(cat "$1" | sed -e '/^---$/,/^---$/d' | pandoc --lua-filter="$DIR/../filters/fix_images.lua" --lua-filter="$DIR/../filters/fix_links.lua")"
+body="$(cat "$1" | sed -e '/^---$/,/^---$/d' | pandoc --lua-filter="$DIR/../filters/fix_md.lua")"
 
 # TODO: this is kind of obscure. any simpler way to pass this path into the template?
 echo "path: ${1/assets\/posts\//}"
